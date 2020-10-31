01fe3d3ca32f0bdae372745a7c6197a6fabecbc6
edith amadi <edithmcamadi5@gmail.com>
2020-10-31 18:31:12 +0300

    modified files

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const container = document.getElementById("container")
 function createGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows)
     container.style.setProperty('--grid-cols', cols)
-    for (count = 0; count < (rows * cols); count++) {
+    for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div")
 
         //change color on mouseover
         cell.addEventListener('mouseover', function(event){
-            event.target.style.backgroundcolor - 'black'
+            event.target.style.backgroundcolor = 'orange'
         })
 
-        
-        container.appendChild(cell).className - "grid-Item"       
+        //set dynamic padding for each cell
+        cell.style.padding = 1, 1
+
+        //append each cell to the container div
+        container.appendChild(cell).className = "grid-Item"       
     };
 }
 createGrid(16,16);
